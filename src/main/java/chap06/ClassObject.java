378ac848fc3979b9a55d900d0a7ebc3d474b27e1
lsh9084 <sanghun1101088@gmail.com>
2024-05-21 11:33:55 +0900

    .

---
+++ src/main/java/chap06/ClassObject.java
@@ -0,0 +1,31 @@
+package chap06;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class ClassObject {
+	public static void main(String[] args) {
+		MyObject obj1 = new MyObject();
+		
+		String A = new String("길순이");
+		String B = "길순이";
+		String C = "길순이";
+		
+		System.out.println(B==C);
+		System.out.println(A.equals(B));
+		obj1.setName("홍길동");
+		System.out.println(obj1);
+		
+	}
+}
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+class MyObject {
+	String name ;
+}
