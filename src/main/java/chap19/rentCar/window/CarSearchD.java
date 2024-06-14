b9551a66e7ac71e65866ea5e4619e1244f9610ed
lsh9084 <sanghun1101088@gmail.com>
2024-06-14 18:40:06 +0900

    .

--- src/main/java/chap19/rentCar/window/CarSearchD.java
+++ src/main/java/chap19/rentCar/window/CarSearchD.java
@@ -1,6 +1,8 @@
 package chap19.rentCar.window;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
@@ -9,6 +11,7 @@ import javax.swing.JTable;
 import chap19.common.RentTableModel;
 import chap19.rentCar.controller.CarController;
 import chap19.rentCar.controller.CarControllerImpl;
+import chap19.rentCar.vo.CarVo;
 
 public class CarSearchD extends JDialog{
 	JTable carTable;
@@ -22,17 +25,69 @@ public class CarSearchD extends JDialog{
 	Object [][] carItems = new String[0][5];
 	CarController carCon = new CarControllerImpl();
 	RentTableModel rm;
-	
+	String iii;
 	public CarSearchD(CarController carC, String str, String str2) {
 		this.carCon = carC;
 		setTitle(str);
-		System.out.println("===str2");
+		this.iii = str2;
+		System.out.println(iii);
 		
-		init();
+		init i = new init();
+		init2();
 	}
 	
+
+	class init{
+		String car_num = null;
+		String car_name = null;
+		String car_color = null;
+		int car_size = 0;
+		String car_made = null;
+		
+		CarVo co1 = CarVo.builder()
+				.car_num(car_num)
+				.car_name(car_name)
+				.car_color(car_color)
+				.car_size(car_size)
+				.car_made(car_made)
+				.build();
+		
+		List<CarVo> carList = null;
+		
+		public init() {
+			
+			carList = new ArrayList<CarVo>();
+			CarVo vo2 = new CarVo();
+			
+			
+			
+			}
+		}
+		public void loadTableData (List<CarVo> carList) {
+			if(carList!=null && carList.size()!=0) {
+				List<CarVo> list1 = new ArrayList<CarVo>();
+				carItems = new String [carList.size()][5];
+				for (int i=0; i<carList.size(); i++) {
+					CarVo carVo = carList.get(i);
+					carItems[i][0]=carVo.getCar_num();
+					carItems[i][1]=carVo.getCar_name();
+					carItems[i][2]=carVo.getCar_color();
+					carItems[i][3] = String.valueOf(carVo.getCar_size());
+					carItems[i][4]=carVo.getCar_made();
+				}
+				
+				rm = new RentTableModel(carItems, columnNames);
+				carTable.setModel(rm);
+			} else {
+//				message("조회한 정보가 없습니다.");
+				carItems = new Object[0][5];
+				rm = new RentTableModel(carItems, columnNames);
+				carTable.setModel(rm);
+			}
+		}
 	
-	private void init() {
+	
+	private void init2() {
 		carTable = new JTable();
 		
 		rm = new RentTableModel(carItems, columnNames);
@@ -44,4 +99,5 @@ public class CarSearchD extends JDialog{
 		setVisible(true);
 		
 	}
+	
 }
